2e557238706af422801e0e39b85bc7fd5177734c
Ariel Elkin <arielelkin@gmail.com>
2014-06-07 16:57:14 -0700

    update view controller to use ivars

--- Mandolin/ViewController.mm
+++ Mandolin/ViewController.mm
@@ -12,53 +12,48 @@
 #import "Mandolin.h"
 #import "AEBlockChannel.h"
 
-@interface ViewController ()
-@property (nonatomic) AEBlockChannel *myMandolinChannel;
-@property (nonatomic) stk::Mandolin *myMandolin;
-@end
-
-@implementation ViewController
+@implementation ViewController {
+    AEBlockChannel *myMandolinChannel;
+    stk::Mandolin *myMandolin;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
+
     AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-    
+
     NSError *errorAudioSetup = NULL;
     BOOL result = [[appDelegate audioController] start:&errorAudioSetup];
     if ( !result ) {
         NSLog(@"Error starting audio engine: %@", errorAudioSetup.localizedDescription);
     }
 
-    stk::Stk::setRawwavePath([[[NSBundle mainBundle] resourcePath] UTF8String]);
-    
-    self.myMandolin = new stk::Mandolin(400);
-    self.myMandolin->setFrequency(400);
-    
-    self.myMandolinChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp  *time,
-                                                                UInt32 frames,
-                                                                AudioBufferList *audio) {
+    stk::Stk::setRawwavePath([[[NSBundle mainBundle] pathForResource:@"rawwaves" ofType:@"bundle"] UTF8String]);
+
+    myMandolin = new stk::Mandolin(400);
+    myMandolin->setFrequency(400);
+
+    myMandolinChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp  *time,
+                                                           UInt32 frames,
+                                                           AudioBufferList *audio) {
         for ( int i=0; i<frames; i++ ) {
-            
+
             ((float*)audio->mBuffers[0].mData)[i] =
-            ((float*)audio->mBuffers[1].mData)[i] = self.myMandolin->tick();
-            
+            ((float*)audio->mBuffers[1].mData)[i] = myMandolin->tick();
+
         }
     }];
-    
-    [[appDelegate audioController] addChannels:@[self.myMandolinChannel]];
 
+    [[appDelegate audioController] addChannels:@[myMandolinChannel]];
 }
 
--(IBAction)pluckMyMandolin {
-    self.myMandolin->pluck(1);
+- (IBAction)pluckMyMandolin {
+    myMandolin->pluck(1);
 }
 
--(IBAction)changeFrequency:(UISlider *)sender {
-    self.myMandolin->setFrequency(sender.value);
+- (IBAction)changeFrequency:(UISlider *)sender {
+    myMandolin->setFrequency(sender.value);
 }
-
 
 @end
